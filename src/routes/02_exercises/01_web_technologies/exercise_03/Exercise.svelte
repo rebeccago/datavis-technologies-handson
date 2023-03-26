43ce651d2aceb0b7fe302418d2414451191d29cd
rebeccago <rebecca.goesker@gmail.com>
2023-03-26 20:26:18 +0200

    Ex3

---
+++ src/routes/02_exercises/01_web_technologies/exercise_03/Exercise.svelte
@@ -0,0 +1,8 @@
+<p style="font-style: italic">
+    The best visual channels ordered by Mackinlay's ranking:
+</p>
+<ol style="font-style: italic">
+    <li>position</li>
+    <li>length</li>
+    <li>angle</li>
+</ol>

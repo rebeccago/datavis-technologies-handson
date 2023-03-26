c60f9503847230c9794a8f8d7284547bd2fd8f1f
rebeccago <rebecca.goesker@gmail.com>
2023-03-26 21:35:08 +0200

    Ex9

---
+++ src/routes/02_exercises/01_web_technologies/exercise_09/Exercise.svelte
@@ -0,0 +1,8 @@
+<script>
+function circum(radius) {
+    return Math.round(radius*2*3.14).toFixed(2);
+  }
+const x = 5
+</script>
+
+<p>"The circumference of a circle with radius {x}m is {circum(x)} m."</p>

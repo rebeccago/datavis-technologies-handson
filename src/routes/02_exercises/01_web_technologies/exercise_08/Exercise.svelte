78cfb6debee6838ced1056b2a9b62f31e4ca4d76
rebeccago <rebecca.goesker@gmail.com>
2023-03-26 21:33:18 +0200

    Ex8

---
+++ src/routes/02_exercises/01_web_technologies/exercise_08/Exercise.svelte
@@ -0,0 +1,7 @@
+<script>
+    const name = "Rebecca";
+    const list = ["reading"," botany","statistics"]
+</script> 
+
+
+<p>"My name is {name} and these are some of my favourite things: {list}.</p>

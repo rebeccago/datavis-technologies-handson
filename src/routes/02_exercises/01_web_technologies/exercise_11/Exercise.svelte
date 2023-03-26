95859fdb48f50c16dc593698351f6d64cf1f279e
rebeccago <rebecca.goesker@gmail.com>
2023-03-26 21:43:04 +0200

    Ex11

---
+++ src/routes/02_exercises/01_web_technologies/exercise_11/Exercise.svelte
@@ -0,0 +1,6 @@
+<script>
+    import Exercisechild from './Exercise_child.svelte';
+    let Answer = 42;
+</script>
+  
+<div>The answer to the ultimate question of life the universe and everything is <Exercisechild answer={Answer}/></div>
